f2214eb91b628d850e2621be420a2a63d4b0ebff
donmoyero <63630549+donmoyero@users.noreply.github.com>
2026-03-01 06:28:45 +0930

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+body {
+    font-family: Arial;
+    background: #0e0e0e;
+    color: white;
+    text-align: center;
+}
 
+.container {
+    width: 80%;
+    margin: auto;
+}
+
+input {
+    display: block;
+    margin: 10px auto;
+    padding: 10px;
+    width: 300px;
+}
+
+button {
+    padding: 10px 20px;
+    background: gold;
+    border: none;
+    cursor: pointer;
+}
+
+canvas {
+    margin-top: 40px;
+    background: white;
+}
