d5d25272b2352b554e24cd0e3a7ab50f8b2b419a
donmoyero <63630549+donmoyero@users.noreply.github.com>
2026-03-04 10:04:31 +0930

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,7 @@
+const supabaseUrl = "https://vopehiqnduxobtaamrnh.supabase.co";
+const supabaseAnonKey = "sb_publishable_oPR-GdRq7Rz3RvBepMkVQw_b7R6ocA3";
 
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
